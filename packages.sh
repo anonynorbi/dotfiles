d05271c34f80db7dd9beae3995053480d7b71f3f
anonynorbi <bonivarthnorbert@gmail.com>
2022-12-09 15:35:58 +0100

    Added packages.sh to install needed packages for i3wm

---
+++ packages.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo pacman -S --noconfirm i3-gaps i3lock i3status i3blocks dmenu terminator vim tldr firefox archlinux-wallpaper feh git vlc ttf-font-awesome ttf-ubuntu-font-family picom pcmanfm materia-gtk-theme papirus-icon-theme lxappearance arandr rofi ttf-droid playerctl pacman-contrib ttf-unifont xorg-fonts-misc
