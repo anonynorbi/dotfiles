a41cbdb1d0bf164c7b0e4da6b313c19d731a9eab
anonynorbi <96613061+anonynorbi@users.noreply.github.com>
2022-12-17 06:03:03 +0100

    Gitlab commit #11

---
+++ pkginstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install packages from official repos
+sudo pacman -S --needed - < pkglist.txt
+
+# Make yay (pacman wrapper and AUR helper)
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -csi
+cd ..
+
+# Install packages from AUR
+yay -S --needed - < foreignpkglist.txt
