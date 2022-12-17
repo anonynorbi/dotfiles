8d336e4b89966d594f06cfb60eaf670590a7413f
anonynorbi <bonivarthnorbert@gmail.com>
2022-12-17 01:10:06 +0100

    Added yay to be built in pkginstall.sh

--- pkginstall.sh
+++ pkginstall.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# Install packages from official repos
 sudo pacman -S --needed - < pkglist.txt
+
+# Make yay (pacman wrapper and AUR helper)
+git clone https://aur.archlinux.org/yay.git
+cd yay
+make -csi
+
+# Install packages from AUR
 yay -S --needed - < foreignpkglist.txt
